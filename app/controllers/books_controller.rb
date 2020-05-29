d1993b6d444d480b6a8158cb5acabd42bda7cde8
永山奈留未 <k016a1001@gmail.com>
2020-05-29 23:40:58 +0900

    Add Bootstrap

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+
+  def top
+  end
+
+  def about
+  end
+  
+  def index
+  	  @book = Book.new
+  	  @books = Book.all
+  end
+
+  def create
+  	  @book = Book.new(book_params)
+  	  @book.user_id = current_user.user_id
+  	  @book.save
+  	  redirect_to book_path(book.id)
+  end
+
+  def show
+  	  @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  	  @book = Book.find(params[:id])
+  	  @book.destroy
+  	  redirect_to books.path
+  end
+
+  private
+  def book_params
+  	  params.require(:book).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  resources :books do
+  	collection do
+  	get 'top'
+  	get 'about'
+    end
+  end
+
+  root 'books#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
